801d2b79f4d746de3eed119ac0532455b88fda87
Maxim_F <maxim.9408@mail.ru>
2018-12-17 20:57:47 +0300

    add Search

--- packages/books/books.js
+++ packages/books/books.js
@@ -4,7 +4,16 @@ const { validate } = require('jsonschema');
 
 // GET /books
 router.get('/', (req, res) => {
-  const books = db.get('books').value();
+  let books;
+
+  if (req.query.search) {
+    books = db
+      .get('books')
+      .filter({ name: req.query.search })
+      .value();
+  } else {
+    books = db.get('books').value();
+  }
 
   res.json({ status: 'OK', data: books });
 });
@@ -15,10 +24,27 @@ router.get('/:id', (req, res) => {
     .get('books')
     .find({ id: req.params.id })
     .value();
-  console.log({ id: req.params.id });
 
   res.json({ status: 'OK', data: book });
 });
+
+// GET /books/:name
+router.get('/:name', (req, res) => {
+  const book = db
+    .get('books')
+    .find({ name: req.params.name })
+    .value();
+  console.log({ name: req.params.name });
+
+  res.json({ status: book ? 'OK' : 'BAD_REQUEST', data: book });
+});
+
+// // GET /books/genre
+// router.get('/:genre', (req, res) => {
+//   const book = db.get('books').filter({ genre: req.params.genre });
+//
+//   res.json({ status: 'OK', data: book });
+// });
 
 // POST /books
 // router.post('/', (req, res, next) => {
@@ -45,33 +71,33 @@ router.get('/:id', (req, res) => {
 //   res.json({ status: 'OK', data: book });
 // });
 
-// // PATCH /books/:id
-// router.patch('/:id', (req, res, next) => {
-//   // const requestBodySchema = {
-//   //   id: 'path-task',
-//   //   type: 'object',
-//   //   properties: {
-//   //     text: { type: 'string' },
-//   //     isCompleted: { type: 'boolean' },
-//   //   },
-//   //   additionalProperties: false,
-//   //   minProperties: 1,
-//   // };
-//   //
-//   // if (!validate(req.body, requestBodySchema).valid) {
-//   //   next(new Error('INVALID_API_FORMAT'));
-//   // }
+// PATCH /books/:id
+router.patch('/:id', (req, res, next) => {
+  // const requestBodySchema = {
+  //   id: 'path-task',
+  //   type: 'object',
+  //   properties: {
+  //     text: { type: 'string' },
+  //     isCompleted: { type: 'boolean' },
+  //   },
+  //   additionalProperties: false,
+  //   minProperties: 1,
+  // };
+  //
+  // if (!validate(req.body, requestBodySchema).valid) {
+  //   next(new Error('INVALID_API_FORMAT'));
+  // }
 
-//   const book = db
-//     .get('cards')
-//     .find({ id: req.params.id })
-//     .assign(req.body)
-//     .value();
+  const book = db
+    .get('books')
+    .find({ id: req.params.id })
+    .assign(req.body)
+    .value();
 
-//   db.write();
+  db.write();
 
-//   res.json({ status: 'OK', data: book });
-// });
+  res.json({ status: 'OK', data: book });
+});
 
 // // DELETE /books/:id
 // router.delete('/:id', (req, res) => {
